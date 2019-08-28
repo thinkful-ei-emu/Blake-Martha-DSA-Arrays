b479a3a0b8151e639f8aee918c1d5ad3663e4282
Martha Plantz <maplantz89@gmail.com>
2019-08-28 12:30:04 -0700

    only two left!!!

--- drills.js
+++ drills.js
@@ -38,7 +38,55 @@ function mergeArrays(myArr1, myArr2){
   return newArr.sort((a,b)=> a-b);
 }
 
-console.log(URLify('www.thinkful.com /tauh ida parv een'));
-console.log(filterLessThan5([1,3,5,6,7,8]));
-console.log(maxSum([4, 6, -3, 5, -2, 1]));
-//console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+function removeChars(str, chars){
+  let charsArr = [];
+  let strArr=[];
+  let solution=[];
+  let solutionStr = '';
+  for(let i= 0; i< chars.length; i++){
+    charsArr.push(chars.charAt(i));
+  }
+  for(let i = 0; i< str.length; i++){
+    strArr.push(str.charAt(i));
+  }
+  strArr.forEach(character => {
+    if(!(charsArr.includes(character))){
+      solution.push(character);
+    }
+  });
+  solution.forEach(character => {
+    solutionStr += character;
+  });
+  return solutionStr;
+}
+
+function products(arr){
+  let total=1; 
+  let solutions = [];
+  arr.forEach(number => {
+    total *= number;
+  });
+  arr.forEach(number => {
+    let newNum = total/number;
+    solutions.push(newNum);
+  });
+
+  // let solutions = [];
+  // arr.forEach((number, index) =>{
+  //   let products = 1;
+  //   for(let i = 0; i< arr.length; i++){
+  //     products = number * products;
+  //     console.log(products);
+  //   }
+  //   products = products/number;
+  //   solutions.push(products);
+  // });
+  return solutions;
+}
+
+// console.log(URLify('www.thinkful.com /tauh ida parv een'));
+// console.log(filterLessThan5([1,3,5,6,7,8]));
+// console.log(maxSum([4, 6, -3, 5, -2, 1]));
+// console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+// console.log(removeChars('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'));
+// console.log(products([1, 3, 9, 4]));
